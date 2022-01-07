91678b0d2f0e389ff7b43ad829e2a055410601eb
SoYi Jeon <soyijeon47@gmail.com>
2022-01-07 23:53:33 +0900

    실습: 1.4 statement() 함수 쪼개기 - 적립 포인트 계산 코드 추출하기

--- practice/main.js
+++ practice/main.js
@@ -31,6 +31,14 @@ function statement(invoice, plays) {
     return result;
   }
 
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === playFor(aPerformance).type) 
+      result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
+
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
@@ -42,10 +50,7 @@ function statement(invoice, plays) {
     }).format;
 
   for (let perf of invoice.performances) {
-    // 포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+    volumeCredits += volumeCreditsFor(perf);
 
     // 청구 내역을 출력한다.
     result += `  ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석)\n`;
