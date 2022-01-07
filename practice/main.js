63ec29c22d17bbe36a950023d1e233720adcfe88
SoYi Jeon <soyijeon47@gmail.com>
2022-01-08 00:14:14 +0900

    실습: 1.4 statement() 함수 쪼개기 - volumeCredits 변수 제거하기

--- practice/main.js
+++ practice/main.js
@@ -50,19 +50,36 @@ function statement(invoice, plays) {
     return result;
   }
 
-  let totalAmount = 0;
-  let volumeCredits = 0;
+  // * 반복문의 임시 변수 제거하기
+  // 1. 반복문 쪼개기 - 변수 값이 누적되는 부분을 따로 빼낸다.
+  // 2. 문장 슬라이드하기 - 변수 선언 문장을 반복문 바로 앞으로 옮긴다.
+  // 3. 함수 추출하기 (임시 변수를 질의 함수로 바꾸기)
+  // 4. 변수 인라인하기 - 추출한 함수로 변수를 대체하여 변수 제거
+  function totalVolumeCredits() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += volumeCreditsFor(perf);
+    }
+    return result;
+  }
+
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result;
+  }
+
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-
     // 청구 내역을 출력한다.
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-    totalAmount+= amountFor(perf);
   }
-  result += `총액: ${usd(totalAmount)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
+
+  result += `총액: ${usd(totalAmount())}\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 }
 
